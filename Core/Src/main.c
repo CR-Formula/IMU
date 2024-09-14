dd0bbc5c2a2a1dd47955b0b0f0cd6e1cefeb1b1f
APBashara <alexbashara2002@gmail.com>
2024-09-14 11:00:08 -0500

    Initial Commit

---
+++ Core/Src/main.c
@@ -0,0 +1,13 @@
+/************************************************
+* @file    main.c 
+* @author  APBashara
+* @date    9/2024
+* 
+* @brief   Main Code to run IMU Board
+***********************************************/
+
+#include "stm32f042x6.h"
+
+void main() {
+
+}
